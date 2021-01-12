a9bd8a1d97684c3bd1b3b6f57190bb64bfd26a1e
Will Burke <billyburke1012@gmail.com>
2021-01-12 14:13:13 -0500

    updating team routers and controllers

---
+++ controllers/team.js
@@ -0,0 +1,18 @@
+const {layout} = require('../utils')
+const { User, Team, Membership} = require('../models')
+//const {Team} = require('../models/team')
+
+
+const teamPage = (req,res) => {
+    const {id} = req.body
+    console.log()
+    console.log(id)
+    res.render('forms/createChore', {
+        locals: {
+            title: "Add new chore"
+        },
+        ...layout
+    });
+};
+
+module.exports = { teamPage }
---
+++ routers/team.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const  teamControllers  = require('../controllers/team')
+
+router.get('/', teamControllers.teamPage)
+// router.get('/newTeam', teamControllers.newTeam)
+// router.post('/userhome/newTeam', teamControllers.processTeam)
+// router.get('/contact/:id', listControllers.showContact)
+// router.get('/contact/editContact/:contactId', listControllers.editContact)
+//.post('/editContact/:contactId')
+module.exports = router
